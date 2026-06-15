9cdaf52f9fb83aad7c49e142b56c3ded1eef63a6
michaelallanbernardo <michaelallanbernardo@icloud.com>
2026-06-15 10:13:53 +0800

    your commit message

---
+++ server/index.js
@@ -0,0 +1,34 @@
+const dns = require("dns");
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
+
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const authRoutes = require("./routes/authRoutes");
+require("dotenv").config();
+
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use("/api/auth", authRoutes);
+
+app.get("/api/status", (req, res) => {
+  res.json({ message: "MangaLog API is running" });
+});
+
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB connected");
+
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MongoDB connection error:", err);
+  });
